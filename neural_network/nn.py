5a74a18e2caec1f9ca7efd270727128538f12931
Sandesh0202 <sandeshpargaonkar7@gmail.com>
2023-05-31 14:11:02 +0530

    Dense Layer

--- neural_network/nn.py
+++ neural_network/nn.py
@@ -1,15 +1,25 @@
-inputs = [1,2,3]
-weights1 = [0.2, 0.54, -0.5]
-weights2 = [0.45, 0.32, -0.56]
-weights3 = [0.8, 0.09, -0.43]
+import numpy as np
 
-bias1 = 2
-bias2 = 4
-bias3 = 1
+np.random.seed(0)
 
-output =  [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + bias1,
-           inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + bias2,
-           inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + bias3]
-print(output)
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-#layers
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        # Initialize weights and biases
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        return self.output
+    
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+#print(layer1.output)
+layer2.forward(layer1.output)
+
+print(layer2.output)
