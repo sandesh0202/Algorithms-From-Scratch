064ce6271617600c7c138418e784c0b1a2a76b11
Sandesh0202 <sandeshpargaonkar7@gmail.com>
2023-06-01 23:00:52 +0530

    commit

--- neural_network/nn.py
+++ neural_network/nn.py
@@ -1,10 +1,10 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -15,11 +15,51 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
         return self.output
     
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class CategoricalEntropyLoss(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        ypred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        
+        if len(y_true.shape) == 1:
+            correct_confidences = ypred_clipped[range(samples), y_true]
+            
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(ypred_clipped*y_true, axis=1)
+            
+        negative_log_likelihoods = np.mean(correct_confidences)
+        return negative_log_likelihoods          
+    
+class Activattion_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+        return self.output
+    
+class Activation_softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims= True))
+        probability = exp_values / np.sum(exp_values, axis=1, keepdims= True)
+        self.output = probability
+    
+layer1 = Layer_Dense(2, 3)
+layer2 = Layer_Dense(3, 3)
+activation1 = Activattion_ReLU()
+activation2 = Activation_softmax()
 
 layer1.forward(X)
-#print(layer1.output)
-layer2.forward(layer1.output)
+#print(layer1.output[:5])
+activation1.forward(layer1.output)
+#print(activation1.output[:5])
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
+#print(activation2.output[:5])
 
-print(layer2.output)
+loss_function = CategoricalEntropyLoss()
+loss = loss_function.calculate(activation2.output, y)
+
+print(loss)
